ad261925e5cde2b34826cdcf06f05c57c2e75e58
yannickmacken <yannickmacken@gmail.com>
2022-08-14 19:54:39 +0200

    add css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
-import './Button.css';
+// Below method of importing (as a module) renames the CSS classnames to unique names,
+// to avoid overspilling styles to other elements. They are scoped only to this component.
+import styles from './Button.module.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
